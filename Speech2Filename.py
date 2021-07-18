c4690746526e919c6e1b851ca01f085f33a3a87a
splishsplashvs <43074817+splishsplashvs@users.noreply.github.com>
2021-07-18 17:16:58 -0400

    Create Speech2Filename.py
    Make sure to put an input and output directory on lines 5 and 6. I had to use Python 3.6.

---
+++ Speech2Filename.py
@@ -0,0 +1,30 @@
+import speech_recognition as sr
+import os
+from shutil import copyfile
+
+in_folder = ""
+out_folder = ""
+
+
+for i in os.listdir(in_folder):
+    if '.wav' in i:
+        print(i)
+        # use the audio file as the audio source
+        r = sr.Recognizer()
+        with sr.AudioFile(i) as source:
+            audio = r.record(source)  # read the entire audio file
+
+        try:
+            text = r.recognize_google(audio)
+            text = text.replace('"', '')
+            text = text.replace('.', '')
+            text = text.replace('?', '')
+            text = text.replace('-', '')
+            text = text.replace('!', '')
+            copyfile(i, out_folder + text + '.wav')
+
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+            
